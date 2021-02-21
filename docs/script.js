93e6ed73432cc0d55f670dd2de4092d916a23426
Alessio Giordano <46226677+alessiogiordano@users.noreply.github.com>
2021-02-21 23:27:40 +0100

    Changed <pubDate> to <agShortDate>

--- docs/script.js
+++ docs/script.js
@@ -189,7 +189,7 @@ function AGgetblogposts() {
 					switch(article[j].tagName) {
 						case "title": title = article[j].textContent; break;
 						case "category": category = article[j].textContent; break;
-						case "pubDate": date = article[j].textContent; break;
+						case "agShortDate": date = article[j].textContent; break;
 						case "link": url = article[j].textContent; break;
 						case "enclosure": image = article[j].getAttribute('url'); if(article[j].getAttribute('darkmode') == "true") { darkIsAvailable = true }; break;
 						case "description": description = article[j].textContent; break;
